9d658c1ce2b517e60e7b4c7aad9af09bbdfd3398
Akshitha <akshithaa0412@gmail.com>
2026-03-01 12:52:20 +0530

    Update attacker.py

--- attacker.py
+++ attacker.py
@@ -1,1 +1,16 @@
+import random
 
+def intercept(bits, bases):
+    """
+    Eve performs intercept-resend attack.
+    """
+    eve_bases = [random.choice(['Z', 'X']) for _ in bits]
+    eve_measurements = []
+
+    for bit, basis, eve_basis in zip(bits, bases, eve_bases):
+        if basis == eve_basis:
+            eve_measurements.append(bit)
+        else:
+            eve_measurements.append(random.randint(0, 1))
+
+    return eve_measurements
